b1341d275e0ff6c905e9a203f2efb848474536eb
dylionen <41701026+dylionen@users.noreply.github.com>
2019-07-19 23:55:16 +0200

    Added class with update Object into DB

--- src/main/java/UpdateStudentObject.java
+++ src/main/java/UpdateStudentObject.java
@@ -1,13 +1,8 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
-
-import java.util.List;
-
-
-public class GetStudentClass {
+public class UpdateStudentObject {
     public static void main(String[] args){
         SessionFactory sessionFactory  =  new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -21,32 +16,15 @@ public class GetStudentClass {
             session.beginTransaction();
             Student s = session.get(Student.class,0);
             System.out.println(s);
-            //session.getTransaction().commit();
-
-            System.out.println("Get All students");
 
 
-            List<Student> studentList = session.createQuery("from Student").list();
-            displayStudents(studentList);
-
-
-            System.out.println("Get users, who name is not'Adam'");
-
-            List<Student> studentList1 = session.createQuery("FROM Student t where t.firstName <>'Adam'").list();
-
-
-            displayStudents(studentList1);
+            System.out.println("Update student,set firstName to 'Johan'");
+            s.setFirstName("Johan");
 
             session.getTransaction().commit();
 
         }finally {
             sessionFactory.close();
-        }
-    }
-
-    private static void displayStudents(List<Student> studentList1) {
-        for (Student tmpStudent : studentList1) {
-            System.out.println(tmpStudent);
         }
     }
 }
